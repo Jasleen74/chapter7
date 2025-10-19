ce2a25684dbd0c8a879ef64390488000715d355d
Jasleen74 <preetjee@gmail.com>
2025-10-19 22:05:51 +0530

    pointer arithmetic for integers

---
+++ pointer_Arithmetic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+     int a = 7;
+
+     int * ptr = &a;
+
+     printf("The address of a is : %u\n", &a);
+     printf("The address of a via pointer is : %u\n", ptr);
+
+     ptr ++;
+
+     printf("The new address is : %u\n", ptr);
+
+     ptr -- ;
+
+     printf("The more new address is : %u \n", ptr);
+
+ return 0;
+}
