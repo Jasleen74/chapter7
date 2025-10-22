5246c62e11f671c82cff1dfea57829054953a2d7
Jasleen74 <preetjee@gmail.com>
2025-10-22 12:09:54 +0530

    functions arrays mixy mixy

--- array_to_functions.c
+++ array_to_functions.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 void add_array (int array[]){
-    array[0]= array[0] +1 ;
+    array[2]= array[2] +1 ;
 }
 int main() {
     int myarray[4] = {6,2,3,4};
 
-    printf("the first element is : %d\n", myarray[0]);
+    printf("the first element is : %d\n", myarray[2]);
 
     add_array(myarray);
 
-    printf("the result will be the change in the  element from 6 to %d", myarray[0]);
+    printf("the result will be the change in the  element from 6 to %d", myarray[2]);
      
   
  return 0;
