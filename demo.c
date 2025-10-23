2285e872d069c28099b8715f87a6abadaab247d2
Jasleen74 <preetjee@gmail.com>
2025-10-23 19:12:54 +0530

    To prove that arr[i] == *(arr + i)

---
+++ demo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+     int arr [4] = {1,2,7,9};
+
+  
+     printf("To prove arr[0]+i = arr[i]\n");
+     printf("Let i be equal to 2\n");
+    printf("the value is :%d\n", *(arr+2));
+
+    printf("the value comes to be : %d", arr[2]);
+
+
+
+  
+ return 0;
+}
